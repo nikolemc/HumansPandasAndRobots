38a1d4b22835d4732fb552177c28915e629b278c
nikolemc <nikolemcstanley@gmail.com>
2017-03-28 13:29:47 -0400

    Human, a Robot, and a Panda  class iwht display name and display greeting

---
+++ HumansPandasAndRobots/HumansPandasAndRobots/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasAndRobots
+{
+    class Human
+    {
+        public string Name { get; set; }
+
+        public void DisplayName()
+        {
+            Console.WriteLine($"Hello, my name is : {this.Name}");
+        }
+
+        public void DisplayGreeting()
+        {
+            Console.WriteLine("I am a Human");
+        }
+    }
+}
---
+++ HumansPandasAndRobots/HumansPandasAndRobots/Panda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasAndRobots
+{
+    class Panda
+    {
+        public string Name { get; set; }
+
+        public void DisplayName()
+        {
+            Console.WriteLine($"Hello, my name is : {this.Name}");
+        }
+
+        public void DisplayGreeting()
+        {
+            Console.WriteLine($"I am a: {Panda}");
+        }
+    }
+
+}
